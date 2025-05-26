126f97bb33e6827336258d635ae92b1a9d23eed5
duyle1062 <22520315@gm.uit.edu.vn>
2025-05-26 07:50:05 +0700

    Done auth of /api/admin/book/update

--- src/main/java/controller/admin/AdminBookUpdateController.java
+++ src/main/java/controller/admin/AdminBookUpdateController.java
@@ -3,6 +3,8 @@ package controller.admin;
 import controller.BaseServlet;
 import model.BookUpdateRequest;
 import service.BookService;
+import util.AuthUtil;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,6 +16,25 @@ import java.util.Map;
 public class AdminBookUpdateController extends BaseServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        if (!AuthUtil.isAdminCookie(req)) {
+            resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            sendJsonResponse(resp, "Admin cookie not found");
+            return;
+        }
+
+        // Lấy admin_id từ cookie
+        String adminIdStr = AuthUtil.getCookieValue(req);
+        int adminId;
+
+        try {
+            assert adminIdStr != null;
+            adminId = Integer.parseInt(adminIdStr);
+        } catch (NumberFormatException e) {
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            sendJsonResponse(resp, "Invalid admin ID in cookie");
+            return;
+        }
+
         BookUpdateRequest request = parseJsonRequest(req, BookUpdateRequest.class);
         if (request.getBookId() <= 0 ||
                 request.getTitle() == null || request.getTitle().trim().isEmpty() ||
